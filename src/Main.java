8dad2b86833d17c65deac28ae3e27a0790b2251c
Ramazan Alipov <ram.alipov@yandex.ru>
2022-12-25 21:16:40 +0300

    Change commit

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,5 @@ public class Main {
         System.out.println((potatoes / eaters) + " картофелин(а)");
         System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
         System.out.println((spices / eaters) + " ложек(ка) специй");
-
     }
 }
